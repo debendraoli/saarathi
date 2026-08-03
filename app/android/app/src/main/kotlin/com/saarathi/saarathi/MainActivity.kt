d86627d5a82b5d699dcda8e6ad10a663fd0491fc
Debendra Oli <debendraoli@pm.me>
2026-08-03 16:57:18 +0545

    app: android/ scaffold (perms, cleartext, saarathi:// deep link, foreground service, compileSdk 36 + desugaring)

---
+++ app/android/app/src/main/kotlin/com/saarathi/saarathi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.saarathi.saarathi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ app/android/build.gradle.kts
@@ -0,0 +1,36 @@
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    // Some plugins (e.g. flutter_webrtc) pull in AndroidX libs that require
+    // compileSdk >= 34, but this Flutter version defaults plugin modules to 31.
+    // Force every Android subproject to compile against a modern SDK.
+    // Registered before evaluationDependsOn so it isn't added post-evaluation.
+    afterEvaluate {
+        val androidExt = extensions.findByName("android")
+        if (androidExt != null) {
+            androidExt.withGroovyBuilder {
+                "compileSdkVersion"(36)
+            }
+        }
+    }
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
